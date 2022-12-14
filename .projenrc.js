ca1688477d84baf78f38888246b569a5823d3c5d
Nathan Dawson <nathan.dawson@thebodycoach.com>
2022-12-14 14:09:17 +0000

    chore: make npm package public

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,12 @@
 const { awscdk } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'oatelaus',
   authorAddress: 'oatelaus@gmail.com',
   cdkVersion: '2.43.1',
   constructsVersion: '10.1.121',
   defaultReleaseBranch: 'main',
+  npmAccess: NpmAccess.PUBLIC,
   name: '@oatelaus/rds-database-scheduler',
   repositoryUrl: 'https://github.com/Oatelaus/rds-database-scheduler.git',
   bundledDeps: [
