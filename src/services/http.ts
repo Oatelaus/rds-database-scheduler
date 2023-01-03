e2308f9f74914e506edeb0e577282f8f0ffb1888
Nathan Dawson <nathan.dawson@thebodycoach.com>
2023-01-03 13:25:11 +0000

    linting issues

--- src/services/http.ts
+++ src/services/http.ts
@@ -8,25 +8,26 @@ export function post(url: string | undefined, data: any) {
 
     const uri = new URL(url);
 
-    const req = request({
-      method: 'POST',
-      host: uri.host,
-      path: uri.pathname,
-      headers: {
-        'Content-Type': 'application/json',
+    const req = request(
+      {
+        method: 'POST',
+        host: uri.host,
+        path: uri.pathname,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    }, ((res) => {
-      let buffers: any[] = [];
-      res.on('error', reject);
-      res.on('data', (buffer) => buffers.push(buffer));
-      res.on(
-        'end',
-        () =>
+      (res) => {
+        let buffers: any[] = [];
+        res.on('error', reject);
+        res.on('data', (buffer) => buffers.push(buffer));
+        res.on('end', () =>
           res.statusCode === 200
             ? resolve(Buffer.concat(buffers).toString())
-            : reject(Buffer.concat(buffers).toString()),
-      );
-    }));
+            : reject(Buffer.concat(buffers).toString())
+        );
+      }
+    );
 
     req.write(JSON.stringify(data));
     req.end();
